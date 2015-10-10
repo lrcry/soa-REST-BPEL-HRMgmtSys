2964d767d1574dd45e703f8e680776860c86f263
hansmong <lrcry@vip.qq.com>
2015-10-10 15:55:33 +1100

    update DAO with public, update controller with parameters

--- src/main/java/au/com/ors/rest/user/controller/UserController.java
+++ src/main/java/au/com/ors/rest/user/controller/UserController.java
@@ -2,24 +2,28 @@ package au.com.ors.rest.user.controller;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import au.com.ors.rest.user.bean.User;
+import au.com.ors.rest.user.dao.UserDAO;
 
 @Controller
 public class UserController {
+	@Autowired
+	UserDAO userDao;
 	
 	@RequestMapping(value = "/users")
 	@ResponseBody
-	public HttpEntity<User> user() {
-		User user = new User("001", "001", "shortKey", "lastName", "firstName", "role", "dept");
-		user.add(linkTo(methodOn(UserController.class).user()).withSelfRel());
-		
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+	public HttpEntity<User> users(@RequestParam(name = "_uid", required = false) String _uid) {
+		User userById = userDao.findByUid(_uid);
+		userById.add(linkTo(methodOn(UserController.class).users(_uid)).withSelfRel());
+		return new ResponseEntity<User>(userById, HttpStatus.OK);
 	}
 }
--- src/main/java/au/com/ors/rest/user/dao/UserDAO.java
+++ src/main/java/au/com/ors/rest/user/dao/UserDAO.java
@@ -5,12 +5,12 @@ import java.util.List;
 import au.com.ors.rest.user.bean.User;
 
 public class UserDAO {
-	User findByUid(String uid) {
+	public User findByUid(String uid) {
 		// TODO
 		return null;
 	}
 	
-	List<User> findAll() {
+	public List<User> findAll() {
 		// TODO
 		return null;
 	}
