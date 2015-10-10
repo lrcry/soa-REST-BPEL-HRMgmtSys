8c3479f229d93ee676b5ff13c7ff1ae3f1fdfce0
hansmong <lrcry@vip.qq.com>
2015-10-10 20:13:02 +1100

    Complete 80% of Users Add exception handler to UserController Complete UserDAO and UserSAXHandler Handle not found user error Add several commons and exceptions

---
+++ src/main/java/au/com/ors/rest/bean/UserError.java
@@ -0,0 +1,31 @@
+package au.com.ors.rest.bean;
+
+import java.io.Serializable;
+
+public class UserError implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7454501798800795164L;
+	
+	private String errCode;
+	
+	private String errMessage;
+
+	public String getErrCode() {
+		return errCode;
+	}
+
+	public void setErrCode(String errCode) {
+		this.errCode = errCode;
+	}
+
+	public String getErrMessage() {
+		return errMessage;
+	}
+
+	public void setErrMessage(String errMessage) {
+		this.errMessage = errMessage;
+	}
+}
--- src/main/java/au/com/ors/rest/dao/UserDAO.java
+++ src/main/java/au/com/ors/rest/dao/UserDAO.java
@@ -2,6 +2,7 @@ package au.com.ors.rest.dao;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
@@ -22,28 +23,36 @@ import au.com.ors.rest.exceptions.DAOException;
 import au.com.ors.rest.exceptions.DAOLoadingXmlFileException;
 import au.com.ors.rest.exceptions.DAOParseXmlException;
 
+/**
+ * DAO for registered users<br/>
+ * 
+ * @author hansmong
+ *
+ */
 @Component
 public class UserDAO {
 	@Resource(name = "dataProperties")
 	private Properties dataProperties;
-	private DefaultHandler userXmlSaxHandler;
-	
-	private static List<User> usersList;
-	
+	private UserSAXHandler userXmlSaxHandler;
+
+	private List<User> usersList = new ArrayList<>();
+
 	@PostConstruct
 	public void init() throws DAOException {
 		// get user data file path
 		String dataPath = dataProperties.getProperty("data.userdata.path");
 		String dataUrl = getClass().getResource(dataPath).getFile();
 		if (StringUtils.isEmpty(dataUrl)) {
-			throw new DAOException("Cannot find data.userdata.path in properties file.");
+			throw new DAOException(
+					"Cannot find data.userdata.path in properties file.");
 		}
-		
+
 		File userDataFile = new File(dataUrl);
 		if (!userDataFile.exists()) {
-			throw new DAOLoadingXmlFileException("Cannot load user XML file from path " + dataUrl);
+			throw new DAOLoadingXmlFileException(
+					"Cannot load user XML file from path " + dataUrl);
 		}
-		
+
 		// open user XML data via SAX parser
 		SAXParserFactory saxParserFactory = SAXParserFactory.newInstance();
 		SAXParser saxParser;
@@ -51,21 +60,35 @@ public class UserDAO {
 			saxParser = saxParserFactory.newSAXParser();
 			userXmlSaxHandler = new UserSAXHandler();
 			saxParser.parse(userDataFile, userXmlSaxHandler);
+			usersList = userXmlSaxHandler.getUsersList();
 		} catch (ParserConfigurationException | SAXException e1) {
-			throw new DAOParseXmlException("Initializing SAX parser exception: " + e1.getMessage());
+			throw new DAOParseXmlException(
+					"Initializing SAX parser exception: " + e1.getMessage());
 		} catch (IOException e) {
-			throw new DAOParseXmlException("Parsing XML exception: " + e.getMessage());
+			throw new DAOParseXmlException("Parsing XML exception: "
+					+ e.getMessage());
 		}
+
+		System.out
+				.println("================================== print all users ==================================");
+		for (User user : usersList) {
+			System.out.println(user.toString());
+		}
+		System.out
+				.println("================================== print all users ended ==================================");
 	}
-	
+
 	public User findByUid(String uid) {
-		// TODO
+		for (User user : usersList) {
+			if (user.get_uid().equals(uid)) {
+				return user;
+			}
+		}
 		
 		return null;
 	}
-	
+
 	public List<User> findAll() {
-		// TODO
-		return null;
+		return this.usersList;
 	}
 }
